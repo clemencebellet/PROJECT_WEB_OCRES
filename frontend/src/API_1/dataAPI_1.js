5c9b1f0b660a94360dd176cf0a1082c335793c6e
clemencebellet <78761529+clemencebellet@users.noreply.github.com>
2022-11-20 19:09:29 +0100

    ajout icone

--- frontend/src/API_1/dataAPI_1.js
+++ frontend/src/API_1/dataAPI_1.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
 import '../main/components/body.css'
+import logopopu from './surpopulation.png'
 
 
 export default class dataAPI extends React.Component {
     render() {
         return (
-            <body>
-                <img src="API_1/population.jpg" alt="cc" />
-                <tr>
-                    <div className="InfosVille">
-
-                        <td>
-
-                            <th>Population</th>
 
 
-                            {this.props.obj.population}
-                        </td>
+            <div className="InfosVille">
+                <div >
 
-                    </div>
+                    <img className="imgpopu" src={logopopu} alt="fireSpot" height="25px" width="30px" /> Population
+
+                </div>
+                <span >
+                    {this.props.obj.population}
+                </span>
 
 
 
-                </tr>
-            </body >
+                <div >
+
+                    Lattitude
+                    {"       "} | {"       "}
+                    Longitude
+                </div>
+
+                <span >
+                    {this.props.obj.latitude}{"°"}
+                    {"      "} | {"      "}
+                    {this.props.obj.longitude}{"°"}</span>
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
         );
     }
 }
