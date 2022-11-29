55f7b78518dade2b8d6e8eea63214b5c72fd2ec6
Cyprien-h <cyp.hatt@gmail.com>
2022-11-29 08:46:11 +0100

    aa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
-import Content from "./API_1/Appel.js"
-import Content2 from "./API_2/Appel_2.js"
-import Weather from "./Meteo/APIweather"
-import Forecast from "./Meteo/APIforecast"
+import Content from "./API_1/Appel.js";
+import Content2 from "./API_2/Appel_2.js";
+import Weather from "./Meteo/APIweather";
+import Forecast from "./Meteo/APIforecast";
+import Menu from "./Menu/Menu";
 
 
+ 
 
 function App() {
 
   return (
     <div className="App">
-      <Content />
-      <Content2 />
-      <Weather />
-      <Forecast />
-      Essai pour pb enregistrement page
-
-
+      <div className='flex1'>
+       <Menu />
+       </div>
+       <div className='flex2'>
+      <div className='a1'><Content /></div>
+      <div className='a2'><Content2 /></div>
+      <div className='a3'><Weather /></div>
+      <div className='a4'><Forecast /></div>
+     
+      </div>
     </div>
   );
 }
--- frontend/src/Menu/Menu.js
+++ frontend/src/Menu/Menu.js
@@ -1,19 +1,17 @@
-import React from "react";
-import './menu.css'
-class Menu extends React.Component {
-
-    render() {
-        return (
-            <div>
-
-                <nav>
-                    <ul className="liste">
-                        <li className="items">DashBoard</li>
-                        <li className="items">Modifier l'API</li>
-                    </ul>
-                </nav>
-            </div>
-
-        )
+import { Component } from 'react';
+import './menu.css';
+export default class Menu extends Component{
+    render(){
+    return(
+        <div className='menu'>
+            <h3>CityBoard</h3>
+            <nav>
+                <a href="/#">Dashboard</a>
+                <a href="/#">Change API</a>
+                
+            </nav>
+            
+        </div>
+    );
     }
-}   
+}
