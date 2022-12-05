912bb1cd5f5c06604e2fd753fc9668c8ab2b88e3
clemencebellet <78761529+clemencebellet@users.noreply.github.com>
2022-12-05 11:02:26 +0100

    Changement Nom dossier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Content from "./API_1/Appel.js";
-import Content2 from "./API_2/Appel_2.js";
+import Content from "./API_Population/Appel.js";
+import Content2 from "./API_Avions/Appel_2.js";
 import Weather from "./Meteo/APIweather";
 import Forecast from "./Meteo/APIforecast";
 import Menu from "./Menu/Menu";
@@ -20,13 +20,14 @@ function App() {
     <div className="App">
       <div className='menu'>
         <Menu />
-        {/* <TachesList /> */}
+
       </div>
       <div className='flex2'>
         {/* <div className='a1'><Weather /><Forecast /></div> */}
         {/* <div className='a3'><Content /></div> */}
-        <div className='a5'><Content2 /></div>
+        {/* <div className='a5'><Content2 /></div> */}
         {/* <div className='a4'><Covid /></div> */}
+        <TachesList />
       </div>
       <div className='a6'>6</div>
 
